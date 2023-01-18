1c3ceaa48f4622b47728a83dd48012868e643541
rtmrshr <ratmir.sher@gmail.com>
2023-01-18 17:26:50 +0200

    A little stack

---
+++ Project7/Petions.swift
@@ -0,0 +1,12 @@
+//
+//  Petions.swift
+//  Project7
+//
+//  Created by  ratmir on 18.01.2023.
+//
+
+import Foundation
+
+struct Petions: Codable {
+    var results: [Petition]
+}
--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var petitions = [Petition]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+//        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
     }
+    
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetions = try? decoder.decode(Petions.self, from: json) {
+            petitions = jsonPetions.results
+            print(petitions)
+            tableView.reloadData()
+        }
+    }
+    
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let petition = petitions[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    
 }
 
